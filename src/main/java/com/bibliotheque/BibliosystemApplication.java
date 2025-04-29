f2414e059b5eaa6a6c01dcb942264613ab9d6229
Nourou <nouroudiakho55@gmail.com>
2025-04-29 22:02:31 +0000

    Mon Projet Bibliotéque Système

---
+++ src/main/java/com/bibliotheque/BibliosystemApplication.java
@@ -0,0 +1,13 @@
+package com.bibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliosystemApplication.class, args);
+	}
+
+}
